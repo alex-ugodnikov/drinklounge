5b809b16cd0b7eaa890639078666850ff6a36193
Alejandro Garcia <agarc371@gmail.com>
2020-08-26 20:54:54 -0400

    add global api variable and update .env with api key

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,13 @@
 //Not sure what this is referring to, fired errors when trying to load site, may need to be removed - A. Garcia
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    console.log('IronGenerator JS imported successfully!');
+  },
+  false
+);
 
-  console.log('IronGenerator JS imported successfully!');
+//declare global api variable
+const apiUrl = `https://www.thecocktaildb.com/api/json/v1/${process.env.API_KEY}/`;
 
-}, false);
+module.exports = apiUrl;
