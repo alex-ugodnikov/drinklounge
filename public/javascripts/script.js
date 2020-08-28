553195176d4e9dd7d897c83ad31a7ba66c2f51dd
Nick Sayago <nick_s7@aol.com>
2020-08-27 23:45:56 -0400

    added temporary nav

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,5 @@
 //Not sure what this is referring to, fired errors when trying to load site, may need to be removed - A. Garcia
-document.addEventListener(
-  'DOMContentLoaded',
-  () => {
-    console.log('IronGenerator JS imported successfully!');
-  },
-  false
-);
+
 
 //declare global api variable
 const apiUrl = `https://www.thecocktaildb.com/api/json/v1/${process.env.API_KEY}/`;
