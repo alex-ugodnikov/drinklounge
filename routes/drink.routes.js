159a1fd8bbe3558fc31ca613218ed4d306c5e1d3
Alejandro Garcia <agarc371@gmail.com>
2020-08-29 16:44:46 -0400

    continued work on update profile logic (incomplete)

--- routes/drink.routes.js
+++ routes/drink.routes.js
@@ -70,48 +70,58 @@ router.get('/alldrinks', (req, res, next) => {
 router.get('/search', (req, res, next) => {
 
   // Pull variables from search query
-  const { letter, s } = req.query;
+  const {
+    letter,
+    s
+  } = req.query;
 
   // Checking if any search variables exist = run apropriate query
 
   if (letter !== undefined) { // if any letter was clicked
 
     axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`)
-    .then((responseFromAPI) => {
-      // handle success
-      console.log(responseFromAPI);
-      res.render('drinks/search.hbs', { letter:letter, cocktails: responseFromAPI.data.drinks });
-    })
-    .catch((error) => {
-      // handle error
-      console.log(error);
-    });  
+      .then((responseFromAPI) => {
+        // handle success
+        console.log(responseFromAPI);
+        res.render('drinks/search.hbs', {
+          letter: letter,
+          cocktails: responseFromAPI.data.drinks
+        });
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
 
-  } else if (s!=='') {  // if input was submitted
+  } else if (s !== '') { // if input was submitted
     axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${s}`)
-    .then((responseFromAPI) => {
-      // handle success
-      console.log(responseFromAPI);
-      res.render('drinks/search.hbs', { s:s, cocktails: responseFromAPI.data.drinks });
-    })
-    .catch((error) => {
-      // handle error
-      console.log(error);
-    });      
+      .then((responseFromAPI) => {
+        // handle success
+        console.log(responseFromAPI);
+        res.render('drinks/search.hbs', {
+          s: s,
+          cocktails: responseFromAPI.data.drinks
+        });
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      });
 
   } else { // if no option was selected, showing basic search page
-    res.render('drinks/search.hbs');  
+    res.render('drinks/search.hbs');
   }
 });
 
 //GET route - show details for a Random Drink
 
-router.get('/random/', (req, res, next) => {
+router.get('/random', (req, res, next) => {
   axios.get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`).then(responseFromApi => {
     console.log(responseFromApi.data);
-    res.render('drinks/details.hbs', responseFromApi.data);
-  });
+    res.render('drinks/details.hbs', responseFromApi.data.drinks);
+  })
 });
+
 
 // GET route - show the details of a single post
 
