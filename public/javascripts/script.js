dfb815473cf682fbe24884c96c2fbf3445835707
Nick Sayago <nick_s7@aol.com>
2020-08-29 10:01:09 -0400

    changed title

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,3 @@
-//Not sure what this is referring to, fired errors when trying to load site, may need to be removed - A. Garcia
-
 
 //declare global api variable
 const apiUrl = `https://www.thecocktaildb.com/api/json/v1/${process.env.API_KEY}/`;
