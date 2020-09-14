020960d18b8573220f8449a8d3606f50f5d261ce
Nick Sayago <nick_s7@aol.com>
2020-09-14 19:41:41 -0400

    test deploy

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost/blog-dev', {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
