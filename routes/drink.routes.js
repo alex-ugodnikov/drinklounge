39f2a4e16959d2679447d5fa2ef726ae93f5ac37
Alejandro Garcia <agarc371@gmail.com>
2020-09-14 21:29:02 -0400

    heroku deploy fixes, remove nodemon reference in auth.routes.js file

--- routes/drink.routes.js
+++ routes/drink.routes.js
@@ -8,7 +8,9 @@ const Post = require('../models/Post.model');
 // require image uploader
 
 const fileUploader = require('../configs/cloudinary.config');
-const { response } = require('express');
+const {
+  response
+} = require('express');
 
 //require user model
 const User = require('../models/User.model');
@@ -40,7 +42,10 @@ router.get('/alldrinks', (req, res, next) => {
 
 router.get('/search', (req, res, next) => {
   // Pull variables from search query
-  const { letter, s } = req.query;
+  const {
+    letter,
+    s
+  } = req.query;
 
   // Checking if any search variables exist = run apropriate query
 
@@ -94,8 +99,8 @@ router.get('/random', (req, res, next) => {
       const drinkId = responseFromApi.data.drinks[0].drinkid;
       //console.log(responseFromApi.data.drinks[0].idDrink);
       Drink.findOne({
-        drinkId
-      })
+          drinkId
+        })
         .populate('author comments')
         .populate({
           // we are populating author in the previously populated comments
@@ -128,8 +133,8 @@ router.get('/drinks/:id', (req, res, next) => {
 
   axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`).then(responseFromApi => {
     Drink.findOne({
-      drinkId
-    })
+        drinkId
+      })
       .populate('author comments')
       .populate({
         // we are populating author in the previously populated comments
@@ -181,12 +186,14 @@ router.get('/drinks/:id', (req, res, next) => {
 ////////////////////////////////////////////////////
 
 router.post('/drinks/:drinkId/addFavorite', (req, res, next) => {
-  const { drinkId } = req.params;
+  const {
+    drinkId
+  } = req.params;
   User.findByIdAndUpdate(req.session.loggedInUser._id, {
-    $push: {
-      favorites: drinkId
-    }
-  })
+      $push: {
+        favorites: drinkId
+      }
+    })
     .then(newFavorite => {
       console.log(`favorite added: ${newFavorite}`);
       res.redirect('/');
@@ -201,12 +208,14 @@ router.post('/drinks/:drinkId/addFavorite', (req, res, next) => {
 ///////////////////////////////////////////////////////
 
 router.post('/drinks/:drinkId/removeFavorite', (req, res, next) => {
-  const { drinkId } = req.params;
+  const {
+    drinkId
+  } = req.params;
   User.findByIdAndUpdate(req.session.loggedInUser._id, {
-    $pull: {
-      favorites: drinkId
-    }
-  })
+      $pull: {
+        favorites: drinkId
+      }
+    })
     .then(removedFave => {
       // User.save();
       console.log(`favorite removed: ${removedFave}`);
