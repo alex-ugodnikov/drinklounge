be2e571a45cf7b2281d2842c3061ff53f3c000e1
Nick Sayago <nick_s7@aol.com>
2020-09-14 21:03:16 -0400

    commit

--- app.js
+++ app.js
@@ -39,7 +39,6 @@ app.use(cookieParser());
 app.use(bindUserToViewLocals);
 
 const app_name = require('./package.json').name;
-// const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 // Routes middleware
 app.use('/', indexRouter);
